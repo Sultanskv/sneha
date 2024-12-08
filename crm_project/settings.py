d75c9efe6d21637e8e86557eb929f7c340bfae48
Ranjeeteinfo <ranjeeteinfo@gmail.com>
2024-12-08 08:57:10 +0530

    Commit message

--- crm_project/settings.py
+++ crm_project/settings.py
@@ -243,5 +243,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'ittechnology1may2023@gmail.com'  # Your email address
-EMAIL_HOST_PASSWORD = 'uyfg cflv fhhz nmhq'      # Your email password or app-specific password
+EMAIL_HOST_PASSWORD = 'euva beex nuyf edqa'      # Your email password or app-specific password
 
